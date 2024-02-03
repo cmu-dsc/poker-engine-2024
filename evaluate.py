daa3dc1e215aaeec368cb34cb94e305ce9fd6e6e
clementou <clement.h.ou@gmail.com>
2024-02-02 21:57:17 -0500

    evaluate fix

--- evaluate.py
+++ evaluate.py
@@ -8,8 +8,7 @@ def is_trips(hand: List[eval7.Card]) -> bool:
 
 
 def score_for_trips(hand: List[eval7.Card]) -> int:
-    rank = hand[0].rank
-    return 600 + rank_value(rank)
+    return 5000 + high_card_value(hand)
 
 
 def is_straight_flush(hand: List[eval7.Card]) -> bool:
@@ -17,7 +16,7 @@ def is_straight_flush(hand: List[eval7.Card]) -> bool:
 
 
 def score_for_straight_flush(hand: List[eval7.Card]) -> int:
-    return 500 + high_card_value(hand)
+    return 4000 + high_card_value(hand)
 
 
 def is_flush(hand: List[eval7.Card]) -> bool:
@@ -26,16 +25,16 @@ def is_flush(hand: List[eval7.Card]) -> bool:
 
 
 def score_for_flush(hand: List[eval7.Card]) -> int:
-    return 400 + high_card_value(hand)
+    return 3000 + high_card_value(hand)
 
 
 def is_straight(hand: List[eval7.Card]) -> bool:
     ranks = sorted([rank_value(card.rank) for card in hand])
-    return ranks in [list(range(start, start + 3)) for start in range(1, 5)]
+    return ranks[0] + 1 == ranks[1] and ranks[1] + 1 == ranks[2]
 
 
 def score_for_straight(hand: List[eval7.Card]) -> int:
-    return 300 + high_card_value(hand)
+    return 2000 + high_card_value(hand)
 
 
 def is_pair(hand: List[eval7.Card]) -> bool:
@@ -44,16 +43,20 @@ def is_pair(hand: List[eval7.Card]) -> bool:
 
 
 def score_for_pair(hand: List[eval7.Card]) -> int:
-    ranks = [card.rank for card in hand]
-    pair_rank = max(set(hand), key=lambda c: ranks.count(c.rank)).rank
-    return 200 + rank_value(pair_rank)
+    ranks = sorted([rank_value(card.rank) for card in hand])
+    if ranks[0] == ranks[1]:
+        return 1000 + 110 * ranks[0] + ranks[2]
+    else:
+        return 1000 + 110 * ranks[2] + ranks[0]
 
 
 def high_card_value(hand: List[eval7.Card]) -> int:
-    return max(rank_value(card.rank) for card in hand)
+    ranks = sorted([rank_value(card.rank) for card in hand], reverse=True)
+    return 100 * ranks[0] + 10 * ranks[1] + 1 * ranks[2]
 
 
 def evaluate(hand: List[eval7.Card]) -> int:
+    assert len(hand) == 3, "hand must be complete"
     if is_trips(hand):
         return score_for_trips(hand)
     elif is_straight_flush(hand):
@@ -69,4 +72,4 @@ def evaluate(hand: List[eval7.Card]) -> int:
 
 
 def rank_value(rank: str) -> int:
-    return eval7.RANK_TO_INT[rank]
+    return int(rank)
