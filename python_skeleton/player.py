483deae3567f1758d8d2d72101e9d885186839fb
clementou <clement.h.ou@gmail.com>
2024-02-26 13:10:03 -0500

    update player.py docs

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -1,6 +1,9 @@
 """
 Simple example pokerbot, written in Python.
 """
+
+from random import random
+
 from skeleton.actions import Action, CallAction, CheckAction, RaiseAction
 from skeleton.states import GameState, TerminalState, RoundState
 from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
@@ -16,6 +19,12 @@ class Player(Bot):
     def __init__(self) -> None:
         """
         Called when a new game starts. Called exactly once.
+
+        Arguments:
+        Nothing.
+
+        Returns:
+        Nothing.
         """
         pass
 
@@ -24,7 +33,20 @@ class Player(Bot):
     ) -> None:
         """
         Called when a new round starts. Called NUM_ROUNDS times.
+
+        Arguments:
+        game_state: the GameState object.
+        round_state: the RoundState object.
+        active: your player's index.
+
+        Returns:
+        Nothing.
         """
+        # my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        # game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
+        # round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
+        # my_cards = round_state.hands[active]  # your cards
+        # big_blind = bool(active)  # True if you are the big blind
         pass
 
     def handle_round_over(
@@ -32,20 +54,42 @@ class Player(Bot):
     ) -> None:
         """
         Called when a round ends. Called NUM_ROUNDS times.
+
+        Arguments:
+        game_state: the GameState object.
+        terminal_state: the TerminalState object.
+        active: your player's index.
+
+        Returns:
+        Nothing.
         """
+        # my_delta = terminal_state.deltas[active]  # your bankroll change from this round
+        # previous_state = terminal_state.previous_state  # RoundState before payoffs
+        # street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
+        # my_cards = previous_state.hands[active]  # your cards
+        # opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
         pass
 
     def get_action(
         self, game_state: GameState, round_state: RoundState, active: int
-    ) -> Action:
+    ) -> None:
         """
         Where the magic happens - your code should implement this function.
         Called any time the engine needs an action from your bot.
+
+        Arguments:
+        game_state: the GameState object.
+        round_state: the RoundState object.
+        active: your player's index.
+
+        Returns:
+        Your action.
         """
+        # May be useful, but you may choose to not use.
         legal_actions = round_state.legal_actions()  # the actions you are allowed to take
-        street = round_state.street  # 0, 1, or 2 representing pre-flop, flop, or river respectively
+        street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
         my_cards = round_state.hands[active]  # your cards
-        board_cards = round_state.deck.peek(street)  # the board cards
+        board_cards = round_state.deck[:street]  # the board cards
         my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
         opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
         my_stack = round_state.stacks[active]  # the number of chips you have remaining
@@ -55,15 +99,17 @@ class Player(Bot):
         opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
 
         if RaiseAction in legal_actions:
-            min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
-            # Simple strategy: always raise the maximum amount
+           min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+           min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+           max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+
+        if RaiseAction in legal_actions and random() < 0.99:
             return RaiseAction(max_raise)
-        
         if CheckAction in legal_actions:
             return CheckAction()
 
         return CallAction()
-    
+
 
 if __name__ == "__main__":
     run_bot(Player(), parse_args())
