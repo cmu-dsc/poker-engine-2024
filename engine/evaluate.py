605c736642bf62dfb896649ab52b0b39b35d0e0e
S. O <soswal@andrew.cmu.edu>
2024-03-23 02:09:40 -0400

    Update variant slightly
    Remove 3-flush, separate 3-straight and 4-straight

--- engine/evaluate.py
+++ engine/evaluate.py
@@ -12,10 +12,10 @@ straight flush (18): 2d 3d 4d 5d
 trips (216): 6d 6s 6h
 two pair (324): 4s 4h 9d 9h
 4-flush (360): 2h 5h 6h 7h
-3-straight (3186): 6h 7h 8d
-3-flush (3588): 2h 7h 9h
-pair (4998): 2h 2d
-high card (4998): everything else
+4-straight (468): 6h 7h 8d
+3-straight (2907): 6h 7h 8d
+pair (6237): 2h 2d
+high card (7020): everything else
 TOTAL: 17550 combos
 """
 
@@ -40,7 +40,7 @@ class ShortDeck:
 
 
 def is_straight_flush(hand: List[str]) -> bool:
-    return num_flushes(hand) == 4 and num_straights(hand) == 2
+    return is_4flush(hand) and is_4straight(hand)
 
 
 def is_trips(hand: List[str]) -> bool:
@@ -52,36 +52,25 @@ def is_two_pair(hand: List[str]) -> bool:
 
 
 def is_4flush(hand: List[str]) -> bool:
-    return num_flushes(hand) == 4
+    suits = [card[1] for card in hand]
+    return len(set(suits)) == 1
 
 
-def is_straight(hand: List[str]) -> bool:
-    return num_straights(hand) > 0
+def is_4straight(hand: List[str]) -> bool:
+    ranks = [int(card[0]) for card in hand]
+    return max(ranks) - min(ranks) == 3 and len(set(ranks)) == 4
+    
 
-
-def is_3flush(hand: List[str]) -> bool:
-    return num_flushes(hand) == 1
+def is_3straight(hand: List[str]) -> bool:
+    ranks = sorted(int(card[0]) for card in hand)
+    for combo in combinations(ranks, 3):
+        if combo[0] == combo[1] - 1 and combo[1] == combo[2] - 1:
+            return True
+    return False
 
 
 def is_pair(hand: List[str]) -> bool:
     return num_pairs(hand) == 1
-
-
-def num_flushes(hand: List[str]) -> int:
-    ans = 0
-    for combo in combinations(hand, 3):
-        if combo[0][1] == combo[1][1] and combo[1][1] == combo[2][1]:
-            ans += 1
-    return ans
-
-
-def num_straights(hand: List[str]) -> int:
-    ans = 0
-    ranks = sorted(int(card[0]) for card in hand)
-    for combo in combinations(ranks, 3):
-        if combo[0] == combo[1] - 1 and combo[1] == combo[2] - 1:
-            ans += 1
-    return ans
 
 
 def num_pairs(hand: List[str]) -> int:
@@ -103,19 +92,11 @@ def frequent_card_value(hand: List[str]) -> int:
     return sum(rank * (10**i) for i, rank in enumerate(ranks))
 
 
-def find_flush(hand: List[str]) -> List[str]:
-    if hand[0][1] == hand[1][1] or hand[0][1] == hand[2][1]:
-        suit = hand[0][1]
-    else:
-        suit = hand[1][1]
-
-    return list(filter(lambda x: x[1] == suit, hand))
-
-
 def find_straight(hand: List[str]) -> List[str]:
     s1 = list(sorted(hand))[1:4]
     s2 = list(sorted(hand))[0:3]
-    if num_straights(s1) > 0:
+    ranks = list(sorted([int(card[0]) for card in hand]))
+    if ranks[2] - ranks[0] == 2:
         return s1
     else:
         return s2
@@ -131,10 +112,10 @@ def evaluate(hand: List[str], board: List[str]) -> int:
         return 60000 + high_card_value(combined_hand)
     elif is_4flush(combined_hand):
         return 50000 + high_card_value(combined_hand)
-    elif is_straight(combined_hand):
-        return 40000 + high_card_value(find_straight(combined_hand))
-    elif is_3flush(combined_hand):
-        return 30000 + high_card_value(find_flush(combined_hand))
+    elif is_4straight(combined_hand):
+        return 40000 + high_card_value(combined_hand)
+    elif is_3straight(combined_hand):
+        return 30000 + high_card_value(find_straight(combined_hand))
     elif is_pair(combined_hand):
         return 20000 + frequent_card_value(combined_hand)
     else:
